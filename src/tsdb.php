798a57acb36c346b8cf26b1633cd56844e32e1de
Roberto Plácido Teixeira <betobyte@gmail.com>
2015-11-23 04:21:45 -0200

    r72.1
    sec
    betobyte

--- src/tsdb.php
+++ src/tsdb.php
@@ -1,7 +1,7 @@
 <?php
 function linkDatabase() {
-   $link = mysql_connect("localhost", "rptcombr_admin_1", "?Rpt64629402", "rptcombr_s3d-01");
-   mysql_select_db('rptcombr_s3dd_01', $link);
+   $link = mysql_connect("localhost", "user", "password");
+   mysql_select_db('dbname', $link);
    return $link;
 }
 ?>
